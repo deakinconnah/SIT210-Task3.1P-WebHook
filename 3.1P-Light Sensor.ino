e4fb8d716ed75a3474200bedbb734a183535b006
deakinconnah <62587019+deakinconnah@users.noreply.github.com>
2020-03-30 10:06:36 +1100

    Add files via upload

---
+++ 3.1P-Light Sensor.ino
@@ -0,0 +1,35 @@
+
+
+int led1 = A5; 
+int led2 = D7; 
+
+int photoresistor = A0;
+int analogValue;
+
+void setup() {
+
+ 
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(photoresistor, INPUT);
+
+}
+
+
+void loop() {
+
+    analogValue = analogRead(photoresistor);
+    
+    Serial.println(photoresistor);
+    
+    if(analogValue > 20){
+        digitalWrite(led1, LOW);
+        digitalWrite(led2, LOW);
+    } else {
+        digitalWrite(led1, HIGH);
+        digitalWrite(led2, HIGH);
+    }
+
+
+}
+
